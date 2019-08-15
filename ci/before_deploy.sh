5109579023d03da7154f2a19ee182a6e0c3dcf80
Franz Heinzmann (Frando) <frando@unbiskant.org>
2019-08-15 14:58:21 +0200

    Travis, again

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,10 +18,11 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin hello --target $TARGET --release -- -C lto
+    cross build --target $TARGET --release
 
     # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/hello $stage/
+    cp target/$TARGET/release/sonar $stage/
+    cp README.md $stage/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
