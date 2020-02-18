ea772475983340f24ae0ce00f03704ce17bda1d4
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-02-18 18:48:29 +0100

    try/catch in tests

--- test/basic.js
+++ test/basic.js
@@ -27,59 +27,67 @@ test('create and delete index', async t => {
 })
 
 test('basic indexing and query', async t => {
-  const [dir, cleanup] = await tempdir()
-  const catalog = new Sonar(dir)
-  const schema = getSchema()
-  const index = await catalog.openOrCreate('index-name', schema)
-  const docs = getDocs()
-  await index.add(docs)
+  try {
+    const [dir, cleanup] = await tempdir()
+    const catalog = new Sonar(dir)
+    const schema = getSchema()
+    const index = await catalog.openOrCreate('index-name', schema)
+    const docs = getDocs()
+    await index.add(docs)
 
-  let results = await index.query('mundo')
-  t.equal(results.length, 1)
-  let first = results[0]
-  t.equal(first.doc.id[0], 'first2')
-  t.equal(first.snippet, null)
-  // console.log(results[0])
+    let results = await index.query('mundo')
+    t.equal(results.length, 1)
+    let first = results[0]
+    t.equal(first.doc.id[0], 'first2')
+    t.equal(first.snippet, null)
+    // console.log(results[0])
 
-  results = await index.query('more', { snippetField: 'body' })
-  t.equal(results.length, 1)
-  first = results[0]
-  t.equal(first.doc.id[0], 'first1')
-  t.equal(first.snippet, 'hi first tell me <b>more</b>')
-  // console.log(results[0])
-  // console.log(await index.meta())
+    results = await index.query('more', { snippetField: 'body' })
+    t.equal(results.length, 1)
+    first = results[0]
+    t.equal(first.doc.id[0], 'first1')
+    t.equal(first.snippet, 'hi first tell me <b>more</b>')
+    // console.log(results[0])
+    // console.log(await index.meta())
 
-  await catalog.close()
-  await cleanup()
-  console.log('HERE')
-  t.end()
+    await catalog.close()
+    await cleanup()
+    console.log('HERE')
+    t.end()
+  } catch (err) {
+    t.fail(err)
+  }
 })
 
 test('basic indexing and query', async t => {
-  const [dir, cleanup] = await tempdir()
-  const catalog = new Sonar(dir)
-  const schema = getSchema()
-  const index = await catalog.openOrCreate('index-name', schema)
-  const docs = getDocs()
-  await index.add(docs)
-  const query = {
-    query: {
-      bool: {
-        must: [{ term: { body: 'hi' } }],
-        must_not: [{ term: { title: 'world' } }]
+  try {
+    const [dir, cleanup] = await tempdir()
+    const catalog = new Sonar(dir)
+    const schema = getSchema()
+    const index = await catalog.openOrCreate('index-name', schema)
+    const docs = getDocs()
+    await index.add(docs)
+    const query = {
+      query: {
+        bool: {
+          must: [{ term: { body: 'hi' } }],
+          must_not: [{ term: { title: 'world' } }]
+        }
       }
     }
+    console.log('QUERY', query.query)
+    let results = await index.queryJson(query)
+    console.log('RESULTS 1', typeof results, results)
+    t.equal(results.docs.length, 1, 'one result')
+    query.query.bool.must_not[0].term.title = 'foo'
+    console.log('QUERY', query.query)
+    results = await index.queryJson(query)
+    console.log('RESULTS 2', results)
+    t.equal(results.docs.length, 2, 'two result')
+    catalog.close()
+    await cleanup()
+    t.end()
+  } catch (err) {
+    t.fail(err)
   }
-  console.log('QUERY', query.query)
-  let results = await index.queryJson(query)
-  console.log('RESULTS 1', typeof results, results)
-  t.equal(results.docs.length, 1, 'one result')
-  query.query.bool.must_not[0].term.title = 'foo'
-  console.log('QUERY', query.query)
-  results = await index.queryJson(query)
-  console.log('RESULTS 2', results)
-  t.equal(results.docs.length, 2, 'two result')
-  catalog.close()
-  await cleanup()
-  t.end()
 })
